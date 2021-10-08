05efd2c032bf28e4dbbaa85e55368e0aae8de0c9
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2021-10-08 17:22:26 +0800

    add new game

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,30 @@
 
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from "react";
 import Board from "../containers/Board";
 
 export default function Home() {
+	const [data, setData] = useState(Array);
+
+	const handleChange = () => {
+		setData([...data, data.length + 1]);
+		console.log(data)
+	}
+
 	return (
 		<Container fluid>
-			<Row>
-				<Col lg={4} xs={12} md={8}>
-					<Board />
-				</Col>
-				<Col lg={4} xs={12} md={8}>
-					<Board />
-				</Col>
+			<Row className="pt-4 p-2" >
+				<Button onClick={handleChange} variant="primary">Add New Game</Button>
+			</Row>
+			<Row className="pt-4" >
+				{data.map(value => {
+					return (
+						<Col lg={4} xs={12} md={8}>
+							<Board />
+						</Col>
+					)
+				})}
 			</Row>
 		</Container>
 	);
