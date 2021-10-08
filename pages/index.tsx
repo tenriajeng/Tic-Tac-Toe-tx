88473cb599d704ba0d52c9103e9b219f0aa6789d
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2021-10-08 17:30:01 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,13 +18,14 @@ export default function Home() {
 				<Button onClick={handleChange} variant="primary">Add New Game</Button>
 			</Row>
 			<Row className="pt-4" >
-				{data.map(value => {
-					<Col lg={4} xs={12} md={8}>
-						<Board />
-					</Col>
-
+				{data.map((value) => {
+					return (
+						<Col lg={4} xs={12} md={8} >
+							<Board />
+						</Col>
+					)
 				})}
 			</Row>
-		</Container>
+		</Container >
 	);
 }
